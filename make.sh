1d9e93d84976d67da263eeb8630dbe55cfb7e0ce
Oiwane Yuki <drhv8272@gmail.com>
2019-08-03 12:17:04 +0900

    テスト用のmake.sh

--- make.sh
+++ make.sh
@@ -5,9 +5,9 @@ set -eu
 if hash docker 2>/dev/null; then
     cd src
     docker run --rm -it -v "${PWD}":/home/hrp2/sdk/workspace/product korosuke613/etrobo-docker make_src
-    mv -f app ../et2019
+    mv -f app ../orderTest
 else
     cd ..
     make app=product/src
-    mv -f app product/et2019
+    mv -f app product/orderTest
 fi
